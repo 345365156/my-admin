9ba72f58336ddaf2cfce07f30e16d5477fb75be8
[lisen] <[sen.li@kongapi.com]>
2019-04-19 17:54:17 +0800

    scss filter router login

---
+++ src/api/index.js
@@ -0,0 +1,66 @@
+import axios from 'axios'
+import {env} from './config'
+import {Message} from 'element-ui'
+
+export const baseURL = env.deploy.testMode.server; // 请求地址 devMode：开发环境  testMode：测试环境  prodMode：生产环境
+
+function apiAxios(method, url, params, response, contentType) {
+  axios({
+    headers: {
+      'Content-Type': contentType,
+      'Key': localStorage.getItem('userId'),
+      'AccessToken': localStorage.getItem('accessToken')
+    },
+    method: method,
+    url: baseURL + url,
+    data: (method === 'POST' || method === 'PUT') ? JSON.stringify(params) : null,
+    params: (method === 'GET' || method === 'DELETE') ? params : null,
+    timeout: 10000
+  }).then(function (res) {
+    response(res)
+  }).catch(function (err) {
+    response(err)
+  })
+}
+
+// 请求拦截器
+axios.interceptors.request.use(
+  function (config) {
+    // 在发送请求之前做些什么
+    console.log(config);
+  }, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  });
+
+// 响应拦截器
+axios.interceptors.response.use(
+  res => {
+    if (res.status >= 200 && res.status < 300) {
+      return res // 返回
+    } else {
+      Message.error(res.data.result) // 请求失败
+    }
+  }, error => {
+    console.log(error)
+    let errorString = error.toString()
+    if (errorString.includes('timeout')) {
+      Message.error('请求超时')
+    }
+    return Promise.reject(error)
+  })
+
+export default {
+  get: function (url, params, response, contentType) {
+    return apiAxios('GET', url, params, response, contentType = 'application/json;charset=utf-8')
+  },
+  post: function (url, params, response, contentType) {
+    return apiAxios('POST', url, params, response, contentType = 'application/json;charset=utf-8')
+  },
+  put: function (url, params, response, contentType) {
+    return apiAxios('PUT', url, params, response, contentType = 'application/json;charset=utf-8')
+  },
+  delete: function (url, params, response, contentType) {
+    return apiAxios('DELETE', url, params, response, contentType = 'application/json;charset=utf-8')
+  }
+}
--- src/main.js
+++ src/main.js
@@ -3,18 +3,39 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import App from './App'
+import Axios from 'axios'
+import Api from './api/index'
 import router from './router'
 import Element from 'element-ui'
+import i18n from './language/index.js'
+import 'normalize.css/normalize.css'
+import './styles/element-variables.scss'
+import '@/styles/index.scss'
 
-Vue.use(Vuex)
-Vue.use(Element)
+import 'element-ui/lib/theme-chalk/index.css'
+import * as filters from './common/filters'
 
-Vue.config.productionTip = false
+Vue.use(Vuex);
+Vue.use(Element, {
+  size: 'medium', zIndex: 3000,
+  i18n: (key, value) => i18n.t(key, value)
+});
+
+Vue.prototype.axios = Axios;
+Vue.prototype.$api = Api;
+
+// 注册全局过滤器
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+});
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  i18n,
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
-})
+});
